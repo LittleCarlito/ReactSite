d464324f6171ea2a590a0696d7a4d1e0b5a410d1
Steven Meier <littlecarlitogithub@gmail.com>
2025-01-13 18:33:03 -0600

    Tile default visibility fixed

--- src/overlay/grid/grid_tile.tsx
+++ src/overlay/grid/grid_tile.tsx
@@ -1,15 +1,17 @@
-export default function GridTile(is_active: boolean) {
-  // TODO if is_active generate a random color from the enabled color map
-  // TODO Take generated color and set background color to it
+interface GridTIleProps {
+  is_active?: boolean;
+}
+
+export default function GridTile({ is_active }: GridTIleProps) {
   const valid_colors: Array<String> = ['red', 'green', 'yellow', 'blue', 'orange', 'purple', 'pink']
-    const rand_index = Math.floor(Math.random() * 100)
-    const background_color = is_active ? valid_colors[rand_index] : 'transparent'
-    return (
-        <>
-            <div className="tile" style = {{backgroundColor: `${background_color}`}}>
-            </div>
-        </>
-    )
+  const rand_index = Math.floor(Math.random() * valid_colors.length)
+  const background_color = is_active ? valid_colors[rand_index] : 'transparent'
+  return (
+    <>
+      <div className="tile" style = {{backgroundColor: `${background_color}`}}>
+      </div>
+    </>
+  )
 }
 
 
